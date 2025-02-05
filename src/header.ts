2487c2ca00b22146f5a1e88248ca68413de34e24
edch <soderblomed@gmail.com>
2025-02-05 11:38:31 +0100

    style: nav-links styles

--- src/header.ts
+++ src/header.ts
@@ -4,28 +4,28 @@ const logo = document.createElement("div");
 logo.textContent = "LeWeb🖥️";
 
 type NavLink = {
-    text: string;
-    url: string;
-}
+  text: string;
+  url: string;
+};
 
-const navLinks:NavLink[] = [
-    {text: "Hem", url: "#"}, 
-    {text: "Om LeWeb", url: "#"}, 
-    {text: "Kundcase", url: "#"}, 
-    {text: "Kontakta oss", url: "#"}
-]
+const navLinks: NavLink[] = [
+  { text: "Hem", url: "#" },
+  { text: "Om LeWeb", url: "#" },
+  { text: "Kundcase", url: "#" },
+  { text: "Kontakta oss", url: "#" },
+];
 
 const ul = document.createElement("ul");
 
 navLinks.forEach((link) => {
   const li = document.createElement("li");
   const navLink = document.createElement("a");
-    navLink.innerHTML = link.text;
-    navLink.href = link.url;
-    li.appendChild(navLink);
-    ul.appendChild(li);
-    nav.appendChild(ul);
-})
+  navLink.innerHTML = link.text;
+  navLink.href = link.url;
+  li.appendChild(navLink);
+  ul.appendChild(li);
+  navContainer.appendChild(ul);
+});
 
 navContainer.append(logo, ul);
 document.getElementById("header")?.appendChild(navContainer);
